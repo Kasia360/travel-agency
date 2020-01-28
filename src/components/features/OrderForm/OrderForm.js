320083533bb304cdb2cee7b48890b074e4ecbc61
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2020-01-29 00:04:48 +0100

    add order now button

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -7,10 +7,49 @@ import pricing from '../../../data/pricing.json';
 
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
 
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
 
+import settings from '../../../data/settings';
+const sendOrder = (trip, options, tripCost) => {
+const totalCost = formatPrice(calculateTotal(tripCost, options));
+const payload = {
+  ...trip,
+  ...options,
+  totalCost,
+};
 
-const OrderForm = ({ tripCost, options, setOrderOption }) => (
+const url = `${settings.db.url}/${settings.db.endpoint.orders}`;
+
+const fetchOptions = {
+  cache: 'no-cache',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(payload),
+};
+
+fetch(url, fetchOptions)
+.then(function (response) {
+      return response.json();
+    }).then(function (parsedResponse) {
+      console.log('parsedResponse', parsedResponse);
+    }).catch(error => console.log('Błąd: ', error));
+};
+
+const isValid = (trip, options, tripCost) => {
+  const { name, contact } = options;
+  if (name !== '' && contact !== '') {
+    sendOrder(trip, options, tripCost);
+  } else { window.alert('Fill name and contact in fields'); }
+};
+
+const OrderForm = ({ trip, tripCost, options, setOrderOption }) => {
+  console.log(options);
+  return (
   <Row>
     {pricing.map(option => (
       <Col key={option.id} md={5}>
@@ -18,15 +57,18 @@ const OrderForm = ({ tripCost, options, setOrderOption }) => (
       </Col>
     ))}
     <Col xs={12}>
-      <OrderSummary tripCost={tripCost} options={options}></OrderSummary>
-    </Col>
-  </Row>
-);
+      <OrderSummary tripCost={tripCost} options={options} ></OrderSummary>
+      <Button onClick={() => isValid(trip, options, tripCost)}>Order now!</Button>
+
+      </Col>
+      </Row>);
+    };
 
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  trip: PropTypes.object,
 };
 
 export default OrderForm;
